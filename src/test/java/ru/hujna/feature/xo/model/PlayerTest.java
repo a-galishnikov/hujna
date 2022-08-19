4dd23618aa54d96b1f9ad56065d5c8d20dd97f91
swtwfrs <partyy@iu-9.info>
2022-08-20 00:03:28 +0300

    Added some displayNames for tests to check how CI works.

--- src/test/java/ru/hujna/feature/xo/model/PlayerTest.java
+++ src/test/java/ru/hujna/feature/xo/model/PlayerTest.java
@@ -1,27 +1,30 @@
 package ru.hujna.feature.xo.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
 
     @Test
+    @DisplayName("Конструктор не должен принимать пустую клетку как ход")
     void constructorShouldNotTakeE() {
         assertThrows(IllegalArgumentException.class, () -> new Player(1, XO.E, true));
     }
 
     @Test
+    @DisplayName("Конструктор падает при пустых параметрах")
     void constructorShouldNotTakeNullXO() {
         assertThrows(IllegalArgumentException.class, () -> new Player(1, null, true));
     }
 
     @Test
+    @DisplayName("Конструктор должен отработать, когда всё есть")
     void constructorShouldWork() {
         var player = new Player(1, XO.X, true);
         assertEquals(1, player.getUserId());
         assertEquals(XO.X, player.getXo());
-        assertEquals(true, player.isGameStarter());
+        assertTrue(player.isGameStarter());
     }
 }
